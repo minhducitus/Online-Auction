764c2672a37d86705b66a6dcec15272a89c0a4e8
DucMinhCao <minhduc.itus@gmail.com>
2019-12-23 09:42:50 +0700

    update db script

--- hostweb/utils/db.js
+++ hostweb/utils/db.js
@@ -1,20 +1,129 @@
 var mysql = require('mysql');
+var q = require('q');
 
-function createConnection() {
-    return mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'Caominhduc123',
-        database: 'auction_minhduc'
+var HOST = "localhost",
+    DB = "myauction",
+    USER = "root",
+    PWD = "Caominhduc123";
+
+// load, insert, update, loadOne, delete
+exports.load = function(sql) {
+    var d = q.defer();
+
+    var connection = mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PWD,
+        database: DB
     });
+
+    connection.connect();
+
+    connection.query(sql, function(error, rows, fields) {
+        if (err){
+            d.reject(error);
+        }
+        d.resolve(rows);
+    });
+
+    connection.end();
+    return d.promise;
 }
 
-var connection = createConnection().connect(function(err) {
-    if (err) {
-        throw err;
-    }
-    console.log('connected');
-})
+exports.insert = function(sql) {
+    var d = q.defer();
+
+    var connection = mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PWD,
+        database: DB
+    });
+
+    connection.connect();
+
+    connection.query(sql, function(error, value) {
+        if (error) {
+            d.reject(error);
+        } else {
+            d.resolve(value.insertId);
+        }
+    });
+
+    connection.end();
+    return d.promise;
+}
+
+exports.update = function(sql) {
+    var d = q.defer();
+
+    var connection = mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PWD,
+        database: DB
+    });
+
+    connection.connect();
+
+    connection.query(sql, function(error, value) {
+        if (error) {
+            d.reject(error);
+        } else {
+            d.resolve(value.changedRows);
+        }
+    });
+
+    connection.end();
+    return d.promise;
+}
+
+exports.delete = function(sql) {
+    var d = q.defer();
+
+    var connection = mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PWD,
+        database: DB
+    });
+
+    connection.connect();
+
+    connection.query(sql, function(error, value) {
+        if (error) {
+            d.reject(error);
+        } else {
+            d.resolve(value.affectedRows);
+        }
+    });
+
+    connection.end();
+    return d.promise;
+}
+
+exports.loadOne = function(sql) {
+    var d = q.defer();
+
+    var connection = mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PWD,
+        database: DB
+    });
+
+    connection.connect();
+    
+    connection.query(sql, function(error, rows, fields) {
+        if (error) {
+            d.reject(error);
+        } else {
+            d.resolve(rows[0]);
+        }
+    });
+
+    connection.end();
+    return d.promise;
+}
 
 
